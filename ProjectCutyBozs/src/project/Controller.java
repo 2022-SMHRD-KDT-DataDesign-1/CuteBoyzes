436244dad99c996dcc916ba1b0bcbdc2a8b82ef5
smhrd <smhrd@59.0.234.70>
2023-01-27 12:41:02 +0900

    2023-01-26 // 조승재 // 완성

--- ProjectCutyBozs/src/project/Controller.java
+++ ProjectCutyBozs/src/project/Controller.java
@@ -9,7 +9,6 @@ import javazoom.jl.player.MP3Player;
 
 public class Controller {
 	int now = 0;
-	// bb은 뭘 뜻 하는 거지?
 	boolean bb = true;
 
 	int sum = 0;
@@ -28,8 +27,21 @@ public class Controller {
 	public void join(PlayerDTO dto) {
 		int row = dao.join(dto);
 		// row
-		if (row > 0)
+		if (row > 0) {
 			System.out.println("회원가입 성공!");
+			if (mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(".\\\\bgm\\opening.mp3");
+			try {
+				// 1초 지연하는 코드
+				TimeUnit.SECONDS.sleep(3);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			mp3.stop();
+		}
 		else
 			System.out.println("회원가입 실패!");
 	}
@@ -37,7 +49,6 @@ public class Controller {
 	// 로그인
 	public void login(PlayerDTO dto) {
 		MP3Player mp3 = new MP3Player();
-
 
 		// DAO 객체 생성
 		DAO dao = new DAO();
@@ -51,22 +62,21 @@ public class Controller {
 		boolean res = dao.login(dto);
 
 		if (res) {
-			
 
 			System.out.println("로그인 성공");
 			if (mp3.isPlaying()) {
 				mp3.stop();
 			}
-			mp3.play(".\\\\bgm\\success.mp3");
+			mp3.play(".\\\\bgm\\opening.mp3");
 			try {
 				// 1초 지연하는 코드
-				TimeUnit.SECONDS.sleep(1);
+				TimeUnit.SECONDS.sleep(3);
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			mp3.stop();
-			
+
 			System.out.print("게임시작 Y / N >> ");
 			yn = sc.next();
 			if (mp3.isPlaying()) {
@@ -82,9 +92,7 @@ public class Controller {
 			}
 			mp3.stop();
 
-			
-			
-			if (yn.equals("Y")||yn.equals("y")) {
+			if (yn.equals("Y") || yn.equals("y")) {
 				System.out.println("      들려주는 명대사를 듣고");
 				if (mp3.isPlaying()) {
 					mp3.stop();
@@ -101,7 +109,7 @@ public class Controller {
 				mp3.stop();
 
 				System.out.println("    어떤 영화인지 맞춰보세요!!!");
-				
+
 				if (mp3.isPlaying()) {
 					mp3.stop();
 				}
@@ -117,7 +125,7 @@ public class Controller {
 				mp3.stop();
 
 				System.out.println("(출제범위: CUTEBOY들이 본 영화 위주)");
-				
+
 				if (mp3.isPlaying()) {
 					mp3.stop();
 				}
@@ -139,17 +147,17 @@ public class Controller {
 				Controller ct = new Controller();
 				if (lebel == 1) {
 
-					System.out.println("상 ㅋㅋ");
+					System.out.println("난이도 >> 상");
 					// ct.안에있는 play메소드 호출
 					play_db(dto, 1);
 				} else if (lebel == 2) {
-					System.out.println("중 ㅋㅋ");
+					System.out.println("난이도 >> 중");
 
-					play_db(dto, 1);
+					play_db2(dto, 1);
 
 				} else if (lebel == 3) {
-					System.out.println("하 ㅋㅋ");
-					play_db(dto, 1);
+					System.out.println("난이도 >> 하");
+					play_db3(dto, 1);
 				}
 
 			} else if (yn.equals("N")) {
@@ -169,32 +177,29 @@ public class Controller {
 		Scanner sc = new Scanner(System.in);
 		Random rd = new Random();
 		MP3Player mp3 = new MP3Player();
-        
+
 		// DAO 객체생성 (Controller와 연결해야하기때문에)
 		DAO dao = new DAO();
-		
+
 		// play_db 에 들어가는 답(answer)변수 초기화
 		String answer = "";
 
-		
 		// 테이블의 Score 컬럼에 더해줄값 초기화
 		int sum = 0;
 		// 테이블의 Score 컬럼에 들어갈값 초기화
 		int score = 0;
 		// 랜덤 함수의 최대값 정의
 		int max = 32;
-		
+
 		// 답을 담아줄 값 정의
 		String[] arr = new String[10];
-		
-		// arr 에 들어가는 인덱스 값 변수 
+
+		// arr 에 들어가는 인덱스 값 변수
 		int cnt = 0;
-		
+
 		// 순서를 정해주는 list
 		int[] raarr = new int[10];
 
-		
-		
 		// 중복없이 raarr에 순서를 담아주는 과정
 		for (int i = 0; i < raarr.length; i++) {
 
@@ -216,15 +221,14 @@ public class Controller {
 				mp3.stop();
 			}
 
-			
 			// 랜덤한 값에서도 위치에 맞는 값을 가져오기위해 MovieDTO타입의 dto_movie를 초기화 해준다
 			// = dao.s_movie_dao -> dao에서 만든 sql문을 통해서 값을 가져오게 하기 위해 선언
 			MovieDTO dto_movie = dao.s_movie_dao(raarr[i]);
-			
+
 			// mp3.play(경로==dto_movie.getPath())
 			mp3.play(dto_movie.getPath());
 			try {
-				TimeUnit.SECONDS.sleep(8);  
+				TimeUnit.SECONDS.sleep(8);
 
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -232,18 +236,18 @@ public class Controller {
 			mp3.stop();
 
 			System.out.println("다시 듣겠습니까 ? >> ");
-			
+
 			// 위에 전역변수로 초기화했던 answer을 선언
 			// sc.nextLine() -> 띄어쓰기 인식이라고 하는데 인식이 안됨 ㅋ
- 			answer = sc.nextLine();
+			answer = sc.nextLine();
 
- 			// 다시 듣겠다고 하면
+			// 다시 듣겠다고 하면
 			if (answer.equals("네") || answer.equals("ㅇㅇ")) {
-				// dto_movie에 dao.s_movie_dao() 에 raarr[i] 랜덤하게 넣은 값을 담아줌 
+				// dto_movie에 dao.s_movie_dao() 에 raarr[i] 랜덤하게 넣은 값을 담아줌
 				dto_movie = dao.s_movie_dao(raarr[i]);
 				mp3.play(dto_movie.getPath());
 				try {
-					TimeUnit.SECONDS.sleep(8); //8
+					TimeUnit.SECONDS.sleep(8); // 8
 
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -258,7 +262,7 @@ public class Controller {
 
 			if (arr[cnt].equals(dto_movie.getTitle())) {
 				System.out.println("정답 ㅋㅋ ");
-				
+
 				if (mp3.isPlaying()) {
 					mp3.stop();
 				}
@@ -275,17 +279,30 @@ public class Controller {
 				// 정답일때마다 10점씩 누적 축적
 				score += 10;
 				try {
-					TimeUnit.SECONDS.sleep(2); //2
+					TimeUnit.SECONDS.sleep(2); // 2
 
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			} else {
 				System.out.println("땡 ㅋㅋ");
-				score = 0;                   // dto_movie.getTitle() 로 db에 저장되어있는 타이틀명을 가져온다
+				if (mp3.isPlaying()) {
+					mp3.stop();
+				}
+				mp3.play(".\\\\bgm\\fail.mp3");
+
+				try {
+					// 3초 지연하는 코드
+					TimeUnit.SECONDS.sleep(1);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				mp3.stop();
+				score = 0; // dto_movie.getTitle() 로 db에 저장되어있는 타이틀명을 가져온다
 				System.out.println("정답은 " + dto_movie.getTitle() + "입니당");
 				try {
-					TimeUnit.SECONDS.sleep(2);  //2
+					TimeUnit.SECONDS.sleep(2); // 2
 
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -300,7 +317,7 @@ public class Controller {
 		}
 
 		System.out.println("점수 >> " + sum);
-		
+
 		// dao안에 있는 score_dao(변수1, 변수2)에 값을 보내줌
 		dao.score_dao(sum, dto);
 
@@ -333,8 +350,8 @@ public class Controller {
 				}
 			}
 		}
-		System.out.println("ss");
 		for (int i = 0; i < arr.length; i++) {
+
 			System.out.print("\n" + (i + 1) + "번 문제 ");
 
 			if (mp3.isPlaying()) {
@@ -345,7 +362,7 @@ public class Controller {
 			MovieDTO dto_movie = dao.j_movie_dao(raarr[i]);
 			mp3.play(dto_movie.getPath());
 			try {
-				TimeUnit.SECONDS.sleep(8);
+				TimeUnit.SECONDS.sleep(7);
 
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -360,7 +377,7 @@ public class Controller {
 				dto_movie = dao.j_movie_dao(raarr[i]);
 				mp3.play(dto_movie.getPath());
 				try {
-					TimeUnit.SECONDS.sleep(8);
+					TimeUnit.SECONDS.sleep(7);
 
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -374,6 +391,19 @@ public class Controller {
 
 			if (arr[cnt].equals(dto_movie.getTitle())) {
 				System.out.println("정답 ㅋㅋ ");
+				if (mp3.isPlaying()) {
+					mp3.stop();
+				}
+				mp3.play(".\\\\bgm\\hahaha.mp3");
+
+				try {
+					// 3초 지연하는 코드
+					TimeUnit.SECONDS.sleep(1);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				mp3.stop();
 				score += 10;
 				try {
 					TimeUnit.SECONDS.sleep(2);
@@ -383,6 +413,19 @@ public class Controller {
 				}
 			} else {
 				System.out.println("땡 ㅋㅋ");
+				if (mp3.isPlaying()) {
+					mp3.stop();
+				}
+				mp3.play(".\\\\bgm\\fail.mp3");
+
+				try {
+					// 3초 지연하는 코드
+					TimeUnit.SECONDS.sleep(1);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				mp3.stop();
 				score = 0;
 				System.out.println("정답은 " + dto_movie.getTitle() + "입니당");
 				try {
@@ -410,6 +453,7 @@ public class Controller {
 		MP3Player mp3 = new MP3Player();
 
 		DAO dao = new DAO();
+		Artlist al = new Artlist();
 
 		String answer = "";
 		int max = 12;
@@ -419,51 +463,119 @@ public class Controller {
 		int score = 0;
 		String[] arr = new String[10];
 		int[] raarr = new int[10];
+		String[] artlist = { al.gokseong(), al.senven(), al.inner(), al.leesoonsin(), al.idol(), al.veteran(), al.ssg(),
+				al.yg(), al.kindgirl(), al.yerimi() };
 
-		for (int i = 0; i < raarr.length; i++) {
-			raarr[i] = i+1;
+		while (true) {
 
-		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print("\n" + (i + 1) + "번 문제 ");
+			for (int i = 0; i < raarr.length; i++) {
+				raarr[i] = i + 1;
 
-			if (mp3.isPlaying()) {
-				mp3.stop();
 			}
+			for (int i = 0; i < arr.length; i++) {
+				System.out.print("\n" + (i + 1) + "번 문제 ");
 
-			MovieDTO dto_movie = dao.h_movie_dao(raarr[i]);
-			mp3.play(dto_movie.getPath());
-			try {
-				TimeUnit.SECONDS.sleep(2);
+				if (mp3.isPlaying()) {
+					mp3.stop();
+				}
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			mp3.stop();
-
-			System.out.println("다시 듣겠습니까 ? >> ");
-			answer = sc.nextLine();
-
-			if (answer.equals("네") || answer.equals("ㅇㅇ")) {
-
-				dto_movie = dao.h_movie_dao(raarr[i]);
+				MovieDTO dto_movie = dao.h_movie_dao(raarr[i]);
 				mp3.play(dto_movie.getPath());
 				try {
-					TimeUnit.SECONDS.sleep(2);
+					TimeUnit.SECONDS.sleep(7);
 
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				mp3.stop();
+
+				System.out.println("다시 듣겠습니까 ? >> ");
+				answer = sc.nextLine();
+
+				if (answer.equals("네") || answer.equals("ㅇㅇ") || answer.equals("ㅇ") || answer.equals("응")
+						|| answer.equals("어") || answer.equals("웅") || answer.equals("ㅇㅇㅇ")) {
+
+					dto_movie = dao.h_movie_dao(raarr[i]);
+					mp3.play(dto_movie.getPath());
+					try {
+						TimeUnit.SECONDS.sleep(7);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					mp3.stop();
+				}
+
+				System.out.print("정답입력 >>");
+
+				arr[cnt] = sc.nextLine();
+
+				if (arr[cnt].equals(dto_movie.getTitle())) {
+
+					System.out.println(al.identity());
+					System.out.println();
+					System.out.println();
+					System.out.println(" 정답 입니다 ! ! !             ");
+					System.out.println();
+					System.out.println();
+					if (mp3.isPlaying()) {
+						mp3.stop();
+					}
+					mp3.play(".\\\\bgm\\hahaha.mp3");
+
+					try {
+						TimeUnit.SECONDS.sleep(1);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					mp3.stop();
+					score += 10;
+					try {
+						TimeUnit.SECONDS.sleep(2);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else {
+					System.out.println(" 땡 ㅋ ㅋ              ");
+					if (mp3.isPlaying()) {
+						mp3.stop();
+					}
+					mp3.play(".\\\\bgm\\fail.mp3");
+
+					try {
+						TimeUnit.SECONDS.sleep(1);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					mp3.stop();
+
+					score = 0;
+					System.out.println(artlist[i]);
+					System.out.println();
+					System.out.println();
+					System.out.println(" 정답은 " + dto_movie.getTitle() + "입니다 ! ! !      ");
+					try {
+						TimeUnit.SECONDS.sleep(2);
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+
+				}
+
+				sum += score;
+				cnt++;
 			}
-
-			System.out.print("정답입력 >>");
-
-			arr[cnt] = sc.nextLine();
+			System.out.println();
+			System.out.println();
+			System.out.println("     당신의 ");
 			if (mp3.isPlaying()) {
 				mp3.stop();
 			}
-			mp3.play(".\\\\bgm\\hahaha.mp3");
+			mp3.play(".\\\\bgm\\rule.mp3");
 
 			try {
 				// 3초 지연하는 코드
@@ -473,55 +585,66 @@ public class Controller {
 				e.printStackTrace();
 			}
 			mp3.stop();
+			System.out.println("   점수는 ?!?! ");
+			if (mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(".\\\\bgm\\rule.mp3");
 
-			if (arr[cnt].equals(dto_movie.getTitle())) {
+			try {
+				// 3초 지연하는 코드
+				TimeUnit.SECONDS.sleep(1);
 
-				System.out.println("정답 ㅋㅋ ");
-			
-				score += 10;
-				try {
-					TimeUnit.SECONDS.sleep(2);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			mp3.stop();
+			System.out.println(sum + "점 입니다 ! ! ! !");
+			if (mp3.isPlaying()) {
+				mp3.stop();
+			}
+			mp3.play(".\\\\bgm\\sum.mp3");
+			try {
+				// 1초 지연하는 코드
+				TimeUnit.SECONDS.sleep(3);
 
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			} else {
-				System.out.println("땡 ㅋㅋ");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			mp3.stop();
+			System.out.println();
+
+			dao.score_dao(sum, dto);
+			System.out.println("한번 더 플레이 하시렵니까 ? >> Y / N ");
+			String yorn = sc.next();
+
+			if (yorn.equals("Y")) {
+
 				if (mp3.isPlaying()) {
 					mp3.stop();
 				}
-				mp3.play(".\\\\bgm\\rule.mp3");
-				
-				
-				
-
+				mp3.play(".\\\\bgm\\opening.mp3");
 				try {
-					// 3초 지연하는 코드
-					TimeUnit.SECONDS.sleep(1);
+					// 1초 지연하는 코드
+					TimeUnit.SECONDS.sleep(3);
 
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 				mp3.stop();
-
-				score = 0;
-				System.out.println("정답은 " + dto_movie.getTitle() + "입니당");
+				continue;
+			} else if (yorn.equals("N")) {
+				System.out.println(" 메인 화면으로 돌아갑니다 - !");
 				try {
-					TimeUnit.SECONDS.sleep(2);
+					TimeUnit.SECONDS.sleep(1);
 
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
-
+				break;
 			}
-			
-			sum += score;
-			cnt++;
+
 		}
-		System.out.println("점수 >> " + sum);
-
-		dao.score_dao(sum, dto);
-
 	}
 
 }
--- ProjectCutyBozs/src/project/Main.java
+++ ProjectCutyBozs/src/project/Main.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 import javazoom.jl.player.MP3Player;
 
-public class Main {
+public class Main extends Artlist {
 
 	public static void main(String[] args) {
 		MP3Player mp3 = new MP3Player();
@@ -15,75 +15,119 @@ public class Main {
 		int select = 0;
 		// 컨트롤러 객체 생성
 		Controller ct = new Controller();
+
+		Artlist al = new Artlist();
+		if (mp3.isPlaying()) {
+			mp3.stop();
+		}
+		mp3.play(".\\\\bgm\\countion.mp3");
+
+		try {
+
+			al.main2(args);
+			mp3.stop();
+			
+		} catch (InterruptedException e1) {
+		}
+
+		try {
+			TimeUnit.SECONDS.sleep(2);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println();
+
+
 		while (select != 4) {
-			
-			
+
 			if (mp3.isPlaying()) {
 				mp3.stop();
 			}
 			mp3.play(".\\\\bgm\\intro.mp3");
 			try {
-				TimeUnit.SECONDS.sleep(1);  
+				TimeUnit.SECONDS.sleep(1);
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-			//      .\\bgm\intro.mp3
-			System.out.print("[1]회원가입 [2]로그인 [3]랭킹확인 "
-					+ "[4]프로그램 종료  >> ");
+			System.out.println(
+					" =========================================");
+			// .\\bgm\intro.mp3
+			System.out
+					.print(" [1]회원가입 [2]로그인 [3]랭킹확인 [4]프로그램 종료  >>  ");
+			System.out.println();
+			System.out.println("  =========================================");
+
 			select = sc.nextInt();
 
 			if (select == 1) {
-
-				System.out.print("ID를 입력해주세요 >>");
+				System.out.println("=========================================");
+				System.out.print("ID를 입력해주세요 >> ");
+				System.out.println();
+				System.out.println("=========================================");
 				String id = sc.next();
-				System.out.print("Password를 입력해주세요 >>");
+				System.out.println("=========================================");
+				System.out.print("Password를 입력해주세요 >> ");
+				System.out.println();
+				System.out.println("=========================================");
 				String pw = sc.next();
-				System.out.print("NickName를 입력해주세요 >>");
+				System.out.println("=========================================");
+				System.out.print("NickName를 입력해주세요 >> ");
+				System.out.println();
+				System.out.println("=========================================");
 				String nick = sc.next();
-
+				mp3.stop();
 				PlayerDTO dto = new PlayerDTO(id, pw, nick);
 				ct.join(dto);
 
 			} else if (select == 2) {
-
-				System.out.print("ID를 입력해주세요 >>");
+				System.out.println("=========================================");
+				System.out.print("ID를 입력해주세요 >> ");
+				System.out.println();
+				System.out.println("=========================================");
 				String id = sc.next();
-				System.out.print("Password를 입력해주세요 >>");
+				System.out.println("=========================================");
+				System.out.print("Password를 입력해주세요 >> ");
+				System.out.println();
+				System.out.println("=========================================");
 				String pw = sc.next();
 				mp3.stop();
 				PlayerDTO dto = new PlayerDTO(id, pw);
 				ct.login(dto);
-				if(ct.bb == false) {
+				if (ct.bb == false) {
 					break;
 				}
-				
-				
-				
 
-			} 
-			// 랭킹확인 
-			else if(select ==3) {
+			}
+			// 랭킹확인
+			else if (select == 3) {
 				DAO dao = new DAO();
 				System.out.println("====== 랭킹확인 ======");
-				System.out.println("rank"+"\t"+"nick"+"\t"+"score");
+				System.out.println("rank" + "\t" + "nick" + "\t" + "score");
 				System.out.println("===================");
 				ArrayList<PlayerDTO> list = dao.rank_dao();
-				
-				for(int i =0; i<list.size(); i++) {
-					System.out.print(i+1+"위"+"\t");
-					System.out.print(list.get(i).getNick()+"\t");
-					System.out.print(list.get(i).getScore()+"\t");
+
+				for (int i = 0; i < list.size(); i++) {
+					System.out.print(i + 1 + "위" + "\t");
+					System.out.print(list.get(i).getNick() + "\t");
+					System.out.print(list.get(i).getScore() + "\t");
 					System.out.println();
 				}
 				System.out.println();
 
-				
-			}
-			else if (select == 4) {
+				System.out.println("메인화면으로 돌아가시겠습니까 ? >> Y/N ");
+				String answer = sc.next();
+				if (answer.equals("Y") || answer.equals("y")) {
+					mp3.stop();
+
+					continue;
+				}
+
+			} else if (select == 4) {
 
 				System.out.print("프로그램 종료");
+				mp3.stop();
 				break;
 
 			}
